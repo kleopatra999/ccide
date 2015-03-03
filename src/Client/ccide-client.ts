7543033fae396a53d90d159e8526e9af008ad1f2
Johannes Brosi <me@brosi.me>
2015-03-03 22:22:10 +0100

    added simple output for directory tree

--- src/Client/ccide-client.ts
+++ src/Client/ccide-client.ts
@@ -2,4 +2,29 @@
 
 module CCIDE.Client {
     console.log("CCIDE Client ready");
+
+
+    var buildTree = function(treeData) {
+        var answer = "<ul>";
+        _.forEach(treeData, function(elem: any, key) {
+            answer +="<li>";
+            answer += key;
+            if(elem.directory && elem.subFiles) {
+                answer += buildTree(elem.subFiles);
+            }
+            answer+="</li>";
+        });
+        answer+="</ul>";
+        return answer;
+    };
+
+    $.ajax({
+        url: "/api/fileservice/filetree",
+        dataType: "json",
+        success: function (data) {
+            var code = buildTree(data);
+            $(".filetree").html(code);
+        }
+    });
+
 }
