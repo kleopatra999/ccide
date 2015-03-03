6cb0cdd9878b871cc3260da827efff10ff773f6a
Johannes Brosi <me@brosi.me>
2015-03-03 21:16:18 +0100

    experimenting with recursive (warning: this could get veeery large!)

--- src/Server/Services/FileService/FileTreeService.ts
+++ src/Server/Services/FileService/FileTreeService.ts
@@ -18,38 +18,65 @@ module CCIDE.Server.Services.FileService {
 
         }
 
-        public onRequest (req, res, next) {
-            var dirPath = CCIDE.Server.Bootstrap.CCIDELoader.getInstance().getCLISettings().getWorkspaceDirectory();
+        private _getRecursive(dirPath) : Q.Promise<any> {
 
-            fs.readdir(dirPath, function(error, files: any) {
-                var promises = [];
+            var that = this;
+            var deferred = Q.defer();
+            var promises = [];
+
+
+            fs.readdir(dirPath, function (error, files:any) {
+
 
                 var answer = {};
 
-                _.forEach(files, function(i, key) {
-                    var deferred = Q.defer();
-                    fs.lstat(path.resolve(dirPath + "/" + i), function(err, stat) {
+                _.forEach(files, function (i, key) {
+                    var fileDeferred = Q.defer();
+                    fs.lstat(path.resolve(dirPath + "/" + i), function (err, stat) {
+                        if (err) {
+                            console.error(err);
+                            fileDeferred.resolve(true);
+                            return;
+                        }
                         answer[i] = {
                             stat: stat,
                             file: stat.isFile(),
                             link: stat.isSymbolicLink(),
                             directory: stat.isDirectory()
                         };
-                        deferred.resolve(stat);
+                        if (stat.isDirectory()) {
+                            that._getRecursive(dirPath + "/" + i).then(function(subFiles) {
+                                answer[i].subFiles = subFiles;
+                                fileDeferred.resolve(stat);
+                            });
+                        } else {
+                            fileDeferred.resolve(stat);
+                        }
+
                     });
 
-                    promises.push(deferred.promise);
+                    promises.push(fileDeferred.promise);
                 });
 
-                Q.allSettled(promises).then(function() {
-                    var body = JSON.stringify(answer);
-
-                    res.writeHead(200, {
-                        'Content-Type': 'application/json' });
-
-                    res.write(body, 'utf8');
-                    res.end();
+                return Q.allSettled(promises).then(function () {
+                    deferred.resolve(answer);
                 });
+            });
+
+            return deferred.promise;
+        }
+
+        public onRequest (req, res, next){
+            var dirPath = CCIDE.Server.Bootstrap.CCIDELoader.getInstance().getCLISettings().getWorkspaceDirectory();
+
+            this._getRecursive(dirPath).then(function(answer) {
+                var body = JSON.stringify(answer);
+
+                res.writeHead(200, {
+                    'Content-Type': 'application/json' });
+
+                res.write(body, 'utf8');
+                res.end();
             });
 
         }
